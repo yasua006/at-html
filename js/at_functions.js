6a9fb5c4d1cccf9ecd29226beec27bae8f145d55
yasua006 <156091445+yasua006@users.noreply.github.com>
2026-03-04 17:54:15 +0100

    Lag README & Bedre style spacing
    Lagde README.md tittel
    Brukte \n etter hver property og bracket - ektra
    på rule avslutninger
    Manuelt tab spacing - \t er ikke 4 spaces

--- js/at_functions.js
+++ js/at_functions.js
@@ -2,24 +2,24 @@
 
 
 /**
- * 
+ *
  * @param {HTMLStyleElement} style_elem 
  */
 const default_light_theme = (style_elem) => {
-    style_elem.textContent = "body {background-color: #fff; color: #444}";
+    style_elem.textContent += "body {\n    background-color: #fff;\n    color: #444\n}\n";
 }
 /**
- * 
+ *
  * @param {HTMLStyleElement} style_elem 
  */
 const default_dark_theme = (style_elem) => {
-    style_elem.textContent =
-    "@media (prefers-color-scheme: dark) {body {background-color: #444; color: #fff}}";
+    style_elem.textContent +=
+    "@media (prefers-color-scheme: dark) {\n    body {\n        background-color: #444;\n        color: #fff\n    }\n}\n";
 }
 
 export const default_themes = () => {
     const style_elem = document.createElement('style');
     default_light_theme(style_elem);
     default_dark_theme(style_elem);
-    document.body.appendChild(style_elem);
+    document.head.appendChild(style_elem);
 }
